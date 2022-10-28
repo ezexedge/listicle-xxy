23c21929fe84423d44748207a05fe095333de8db
ezequiel <ezeedge@gmail.com>
2022-10-27 21:00:57 -0300

    add images

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,20 +1,38 @@
 import React, {useState} from 'react';
-import {ScrollView, Text} from 'react-native';
+import {ScrollView, Text,FlatList} from 'react-native';
 import AuthHeader from '../../../components/AuthHeader';
 import Button from '../../../components/Button';
 import Input from '../../../components/Input';
 import Separator from '../../../components/Separator';
 import {styles} from './styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Header from '../../../components/Header';
+import { categories } from '../../../data/categories';
+import CategoryBox from '../../../components/CategoryBox';
 
 const Home = ({navigation}) => {
 
+    const renderCategoryItem = ({ item, index }) => {
+        return (
+            <CategoryBox
+             
+                title={item?.title} 
+                image={item?.image} 
+            />
+        )
+    }
 
   return (
     <SafeAreaView>
-      <ScrollView style={styles.container}>
-        <Text>Home</Text>
-      </ScrollView>
+        <Header showSearch title="Busca" showLogout />
+        <FlatList 
+                style={styles.list} 
+                horizontal 
+                data={categories} 
+                renderItem={renderCategoryItem} 
+                keyExtractor={(item, index) => String(index)} 
+            />
+
     </SafeAreaView>
   );
 };
